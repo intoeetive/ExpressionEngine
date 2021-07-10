cc7766145b0443619eefb0c1cddde3bc4edd269a
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:59:27 +0300

    New translations date_lang.php (Spanish)

--- system/ee/language/Spanish/date_lang.php
+++ system/ee/language/Spanish/date_lang.php
@@ -2,113 +2,113 @@
 
 $lang = array(
 
-    'date_year' => 'Year',
+    'date_year' => 'Año',
 
-    'date_years' => 'Years',
+    'date_years' => 'Años',
 
-    'date_month' => 'Month',
+    'date_month' => 'Mes',
 
-    'date_months' => 'Months',
+    'date_months' => 'Meses',
 
-    'date_week' => 'Week',
+    'date_week' => 'Semana',
 
-    'date_weeks' => 'Weeks',
+    'date_weeks' => 'Semanas',
 
-    'date_day' => 'Day',
+    'date_day' => 'Día',
 
-    'date_days' => 'Days',
+    'date_days' => 'Días',
 
-    'date_hour' => 'Hour',
+    'date_hour' => 'Hora',
 
-    'date_hours' => 'Hours',
+    'date_hours' => 'Horas',
 
-    'date_minute' => 'Minute',
+    'date_minute' => 'Minuto',
 
-    'date_minutes' => 'Minutes',
+    'date_minutes' => 'Minutos',
 
-    'date_second' => 'Second',
+    'date_second' => 'Segundo',
 
-    'date_seconds' => 'Seconds',
+    'date_seconds' => 'Segundos',
 
-    'UM12' => '(UTC -12:00) Baker/Howland Island',
+    'UM12' => '(UTC -12:00) Barbacoa/Isla de cómic',
 
-    'UM11' => '(UTC -11:00) Samoa Time Zone, Niue',
+    'UM11' => '(UTC -11:00) Zona horaria de Samoa, Niue',
 
-    'UM10' => '(UTC -10:00) Hawaii-Aleutian Standard Time, Cook Islands, Tahiti',
+    'UM10' => '(UTC -10:00) Hora Estándar Hawaii-Aleutian, Islas Cook, Tahití',
 
-    'UM95' => '(UTC -9:30) Marquesas Islands',
+    'UM95' => '(UTC -9:30) Islas Marquesas',
 
-    'UM9' => '(UTC -9:00) Alaska Standard Time, Gambier Islands',
+    'UM9' => '(UTC -9:00) Tiempo Estándar de Alaska, Islas Gambier',
 
-    'UM8' => '(UTC -8:00) Pacific Standard Time, Clipperton Island',
+    'UM8' => '(UTC -8:00) Hora estándar del Pacífico, Isla Clipperton',
 
-    'UM7' => '(UTC -7:00) Mountain Standard Time',
+    'UM7' => '(UTC -7:00) Hora estándar de la montaña',
 
-    'UM6' => '(UTC -6:00) Central Standard Time',
+    'UM6' => '(UTC -6:00) Hora estándar central',
 
-    'UM5' => '(UTC -5:00) Eastern Standard Time, Western Caribbean Standard Time',
+    'UM5' => '(UTC -5:00) Hora Estándar del Estándar del Estándar del Caribe Occidental',
 
-    'UM45' => '(UTC -4:30) Venezuelan Standard Time',
+    'UM45' => '(UTC -4:30) Hora Estándar de Venezuela',
 
-    'UM4' => '(UTC -4:00) Atlantic Standard Time, Eastern Caribbean Standard Time',
+    'UM4' => '(UTC -4:00) Hora del Estándar Atlántico, Hora Estándar del Caribe Oriental',
 
-    'UM35' => '(UTC -3:30) Newfoundland Standard Time',
+    'UM35' => '(UTC -3:30) Hora Estándar de Terranova',
 
-    'UM3' => '(UTC -3:00) Argentina, Brazil, French Guiana, Uruguay',
+    'UM3' => '(UTC -3:00) Argentina, Brasil, Guayana, Uruguay',
 
-    'UM2' => '(UTC -2:00) South Georgia/South Sandwich Islands',
+    'UM2' => '(UTC -2:00) Islas Sur/Sur de Sandwich',
 
-    'UM1' => '(UTC -1:00) Azores, Cape Verde Islands',
+    'UM1' => '(UTC -1:00) Azores, Islas de Cabo Verde',
 
-    'UTC' => '(UTC) Greenwich Mean Time, Western European Time',
+    'UTC' => '(UTC) Greenwich Media Time, Western European Time',
 
-    'UP1' => '(UTC +1:00) Central European Time, West Africa Time',
+    'UP1' => '(UTC +1:00) Tiempo de Europa Central, Hora de África Occidental',
 
-    'UP2' => '(UTC +2:00) Central Africa Time, Eastern European Time, Kaliningrad Time',
+    'UP2' => '(UTC +2:00) Tiempo de África Central, Hora de Europa Oriental, Hora de Kalininingrado',
 
-    'UP3' => '(UTC +3:00) Moscow Time, East Africa Time',
+    'UP3' => '(UTC +3:00) Hora de Moscú, Hora de África Oriental',
 
-    'UP35' => '(UTC +3:30) Iran Standard Time',
+    'UP35' => '(UTC +3:30) Hora Estándar de Irán',
 
-    'UP4' => '(UTC +4:00) Azerbaijan Standard Time, Samara Time',
+    'UP4' => '(UTC +4:00) Hora Estándar de Pachayán, Hora Samara',
 
     'UP45' => '(UTC +4:30) Afghanistan',
 
-    'UP5' => '(UTC +5:00) Pakistan Standard Time, Yekaterinburg Time',
+    'UP5' => '(UTC +5:00) Hora Estándar de Pakistán, Hora de Yekaterinburgo',
 
-    'UP55' => '(UTC +5:30) Indian Standard Time, Sri Lanka Time',
+    'UP55' => '(UTC +5:30) Hora Estándar de la India, Hora de Sri Lanka',
 
-    'UP575' => '(UTC +5:45) Nepal Time',
+    'UP575' => '(UTC +5:45) Tiempo Nepal',
 
-    'UP6' => '(UTC +6:00) Bangladesh Standard Time, Bhutan Time, Omsk Time',
+    'UP6' => '(UTC +6:00) Hora Estándar de Bangladesh, Tiempo Bhutan, Tiempo Omsk',
 
-    'UP65' => '(UTC +6:30) Cocos Islands, Myanmar',
+    'UP65' => '(UTC +6:30) Islas Cocos, Myanmar',
 
     'UP7' => '(UTC +7:00) Krasnoyarsk Time, Cambodia, Laos, Thailand, Vietnam',
 
-    'UP8' => '(UTC +8:00) Australian Western Standard Time, Beijing Time, Irkutsk Time',
+    'UP8' => '(UTC +8:00) Hora Estándar del Oeste Australiano, Beijing Time, Irkutsk Time',
 
-    'UP875' => '(UTC +8:45) Australian Central Western Standard Time',
+    'UP875' => '(UTC +8:45) Hora Estándar Central Occidental',
 
-    'UP9' => '(UTC +9:00) Japan Standard Time, Korea Standard Time, Yakutsk Time',
+    'UP9' => '(UTC +9:00) Hora Estándar de Japón, Hora Estándar de Corea, Hora Yakutsk',
 
-    'UP95' => '(UTC +9:30) Australian Central Standard Time',
+    'UP95' => '(UTC +9:30) Hora Estándar Central Australiana',
 
-    'UP10' => '(UTC +10:00) Australian Eastern Standard Time, Vladivostok Time',
+    'UP10' => '(UTC +10:00) Hora Estándar Australiana, Hora de ivostok',
 
-    'UP105' => '(UTC +10:30) Lord Howe Island',
+    'UP105' => '(UTC +10:30) Isla de Howe del Señor',
 
-    'UP11' => '(UTC +11:00) Magadan Time, Solomon Islands, Vanuatu',
+    'UP11' => '(UTC +11:00) Hora de Magadán, Islas Salomón, Vanuatu',
 
-    'UP115' => '(UTC +11:30) Norfolk Island',
+    'UP115' => '(UTC +11:30) Isla Norfolk',
 
-    'UP12' => '(UTC +12:00) Fiji, Gilbert Islands, Kamchatka Time, New Zealand Standard Time',
+    'UP12' => '(UTC +12:00) Fiyi, Islas Gilbert, Kamchatka Time, Nueva Zelanda Hora Estándar',
 
-    'UP1275' => '(UTC +12:45) Chatham Islands Standard Time',
+    'UP1275' => '(UTC +12:45) Tiempo Estándar de las Islas Catham',
 
-    'UP13' => '(UTC +13:00) Phoenix Islands Time, Tonga',
+    'UP13' => '(UTC +13:00) Tiempo de las Islas Fénix, Tonga',
 
-    'UP14' => '(UTC +14:00) Line Islands',
+    'UP14' => '(UTC +14:00) Islas Líneas',
 
 );
 
