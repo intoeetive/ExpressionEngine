696ca53b691b0f446a89fd0c5239aaa2c7071cf3
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:58:38 +0300

    New translations unit_test_lang.php (Spanish)

--- system/ee/language/Spanish/unit_test_lang.php
+++ system/ee/language/Spanish/unit_test_lang.php
@@ -2,42 +2,42 @@
 
 $lang = array(
 
-    'ut_array' => 'Array',
+    'ut_array' => 'Matriz',
 
     'ut_boolean' => 'Boolean',
 
-    'ut_double' => 'Float', // can be the same as float
-    'ut_failed' => 'Failed',
+    'ut_double' => 'Ropa', // can be the same as float
+    'ut_failed' => 'Fallo',
 
-    'ut_file' => 'File Name',
+    'ut_file' => 'Nombre del archivo',
 
-    'ut_float' => 'Float',
+    'ut_float' => 'Ropa',
 
-    'ut_integer' => 'Integer',
+    'ut_integer' => 'Entero',
 
-    'ut_line' => 'Line Number',
+    'ut_line' => 'Número de línea',
 
-    'ut_notes' => 'Notes',
+    'ut_notes' => 'Notas',
 
-    'ut_null' => 'Null',
+    'ut_null' => 'Nulo',
 
-    'ut_object' => 'Object',
+    'ut_object' => 'Objeto',
 
-    'ut_passed' => 'Passed',
+    'ut_passed' => 'Pasado',
 
-    'ut_res_datatype' => 'Expected Datatype',
+    'ut_res_datatype' => 'Tipo de datos esperado',
 
-    'ut_resource' => 'Resource',
+    'ut_resource' => 'Recurso',
 
-    'ut_result' => 'Result',
+    'ut_result' => 'Resultado',
 
-    'ut_string' => 'String',
+    'ut_string' => 'Cadena',
 
-    'ut_test_datatype' => 'Test Datatype',
+    'ut_test_datatype' => 'Tipo de datos de prueba',
 
-    'ut_test_name' => 'Test Name',
+    'ut_test_name' => 'Nombre de prueba',
 
-    'ut_undefined' => 'Undefined Test Name',
+    'ut_undefined' => 'Nombre de prueba indefinido',
 
 );
 
