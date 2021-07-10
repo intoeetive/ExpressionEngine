2f3e8105c3bda45d22fc092822b3da41af1d3d2b
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:57:58 +0300

    New translations jquery_lang.php (Spanish)

--- system/ee/language/Spanish/jquery_lang.php
+++ system/ee/language/Spanish/jquery_lang.php
@@ -6,7 +6,7 @@ $lang = array(
 
     'jquery_module_name' => 'jQuery',
 
-    'missing_jquery_file' => 'The requested jQuery file could not be found.',
+    'missing_jquery_file' => 'El archivo jQuery solicitado no pudo ser encontrado.',
 
 );
 
