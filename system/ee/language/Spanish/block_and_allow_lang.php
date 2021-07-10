787e04ba510064bc35c79570b7344b6e6d802c0a
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:59:22 +0300

    New translations block_and_allow_lang.php (Spanish)

--- system/ee/language/Spanish/block_and_allow_lang.php
+++ system/ee/language/Spanish/block_and_allow_lang.php
@@ -2,95 +2,95 @@
 
 $lang = array(
 
-    'block_and_allow_module_name' => 'Block and Allow',
+    'block_and_allow_module_name' => 'Bloquear y permitir',
 
-    'block_and_allow_module_description' => 'Block/allow IP Addresses, URLs, and User-Agents',
+    'block_and_allow_module_description' => 'Bloquear/permitir direcciones IP, URLs y agentes de usuario',
 
-    'add_htaccess_file' => 'Add to .htaccess file?',
+    'add_htaccess_file' => '¿Añadir al archivo .htacceso?',
 
-    'add_htaccess_file_desc' => 'Full path location of your <code>.htaccess</code> file.',
+    'add_htaccess_file_desc' => 'Ubicación completa de tu archivo <code>.htaccess</code>.',
 
-    'btn_save_list' => 'Save List',
+    'btn_save_list' => 'Guardar lista',
 
-    'btn_download_blockedlist' => 'Download EE Blocked List',
+    'btn_download_blockedlist' => 'Descargar lista bloqueada EE',
 
-    'btn_download_allowedlist' => 'Download EE Allowed List',
+    'btn_download_allowedlist' => 'Descargar lista permitida de EE',
 
-    'btn_save_settings' => 'Save Settings',
+    'btn_save_settings' => 'Guardar ajustes',
 
-    'settings' => 'Settings',
+    'settings' => 'Ajustes',
 
-    'lists' => 'Lists',
+    'lists' => 'Listas',
 
-    'allowedlist' => 'Allowed list',
+    'allowedlist' => 'Lista permitida',
 
-    'allowedlist_desc' => 'Allowed lists <b>allow</b> access to your website via specific <abbr title="Internet Protocol">IP</abbr> address, user agents and <abbr title="Unified Resource Locator">URL</abbr>s.',
+    'allowedlist_desc' => 'Las listas permitidas <b>permiten</b> el acceso a su sitio web a través de <abbr title="Internet Protocol">dirección IP</abbr> específica y agentes de usuario y <abbr title="Unified Resource Locator">URL</abbr>s.',
 
-    'blockedlist' => 'Blocked list',
+    'blockedlist' => 'Lista bloqueada',
 
-    'blockedlist_desc' => 'Blocked lists <b>disallow</b> access to your website via specific <abbr title="Internet Protocol">IP</abbr> address, user agents and <abbr title="Unified Resource Locator">URL</abbr>s.',
+    'blockedlist_desc' => 'Listas bloqueadas <b>no permiten</b> el acceso a su sitio web a través de <abbr title="Internet Protocol">dirección IP</abbr> específica, agentes de usuario y <abbr title="Unified Resource Locator">URL</abbr>s.',
 
-    'ip_address' => '<abbr title="Internet Protocol">IP</abbr> address',
+    'ip_address' => '<abbr title="Internet Protocol">dirección IP</abbr>',
 
-    'ip_address_desc' => 'Place each <abbr title="Internet Protocol">IP</abbr> address on a separate line.<br>You may use <code>*</code> as a wild-card e.g. 123.456.<code>*</code>',
+    'ip_address_desc' => 'Coloca cada <abbr title="Internet Protocol">dirección IP</abbr> en una línea separada.<br>Puedes usar <code>*</code> como comodín ej. 123.456.<code>*</code>',
 
-    'user_agent' => 'User Agent',
+    'user_agent' => 'Agente de usuario',
 
-    'user_agent_desc' => 'Place each user agent on a separate line.',
+    'user_agent_desc' => 'Coloque cada agente de usuario en una línea separada.',
 
     'url' => '<abbr title="Unified Resource Locator">URL</abbr>',
 
-    'url_desc' => 'Place each <abbr title="Unified Resource Locator">URL</abbr> on a separate line.',
+    'url_desc' => 'Coloca cada <abbr title="Unified Resource Locator">URL</abbr> en una línea separada.',
 
-    'htaccess_path_removed' => '.htaccess Path Removed',
+    'htaccess_path_removed' => '.htaccess Ruta eliminada',
 
-    'htaccess_path_removed_desc' => 'The .htaccess file path was removed from your configuration',
+    'htaccess_path_removed_desc' => 'La ruta del archivo .htaccess fue eliminada de su configuración',
 
-    'htaccess_written_successfully' => '.htaccess File Updated',
+    'htaccess_written_successfully' => '.htaccess Archivo actualizado',
 
-    'htaccess_written_successfully_desc' => 'The .htaccess file was written successfully.',
+    'htaccess_written_successfully_desc' => 'El archivo .htaccess se ha escrito correctamente.',
 
-    'invalid_htaccess_path' => 'Invalid Path or Permissions for .htaccess file',
+    'invalid_htaccess_path' => 'Ruta o permisos no válidos para el archivo .htaccess',
 
-    'lists_updated' => 'Lists updated',
+    'lists_updated' => 'Listas actualizadas',
 
-    'lists_updated_desc' => 'The blocked and allowed lists were updated successfully.',
+    'lists_updated_desc' => 'Las listas bloqueadas y permitidas se actualizaron correctamente.',
 
-    'blockedlist_downloaded' => 'The blocked list has been updated by being downloaded from ExpressionEngine.',
+    'blockedlist_downloaded' => 'La lista bloqueada ha sido actualizada al descargarse de ExpressionEngine.',
 
-    'allowedlist_downloaded' => 'The allowed list has been updated by being downloaded from ExpressionEngine.',
+    'allowedlist_downloaded' => 'La lista permitida ha sido actualizada por ser descargada desde ExpressionEngine.',
 
-    'ee_allowedlist' => 'Download ExpressionEngine Allowed List',
+    'ee_allowedlist' => 'Lista permitida para descargar ExpressionEngine',
 
-    'ref_allowedlist_irretrievable' => 'Error: New allowed list was irretrievable.',
+    'ref_allowedlist_irretrievable' => 'Error: La nueva lista permitida era irretrievable.',
 
-    'ref_view_allowedlist' => 'View Allowed List',
+    'ref_view_allowedlist' => 'Ver lista permitida',
 
-    'ref_no_allowedlist' => 'There is currently nothing listed to allow',
+    'ref_no_allowedlist' => 'Actualmente no hay nada listado para permitir',
 
-    'ref_allowedlisted' => 'Listed to allow',
+    'ref_allowedlisted' => 'Lista para permitir',
 
-    'ref_no_allowedlist_table' => 'Allowedlist database table does not exist',
+    'ref_no_allowedlist_table' => 'La tabla de base de datos permitida no existe',
 
-    'ref_type' => 'Item Type',
+    'ref_type' => 'Tipo de artículo',
 
-    'ee_blockedlist' => 'Download ExpressionEngine Blocked List',
+    'ee_blockedlist' => 'Descargar Lista Bloqueada de ExpressionEngine',
 
-    'ref_blockedlist_irretrievable' => 'Error: New Blocked List was irretrievable.',
+    'ref_blockedlist_irretrievable' => 'Error: La nueva lista bloqueada era irretrievable.',
 
-    'ref_view_blockedlist' => 'View Blocked List',
+    'ref_view_blockedlist' => 'Ver lista bloqueada',
 
-    'ref_no_blockedlist' => 'There is currently nothing listed to block',
+    'ref_no_blockedlist' => 'Actualmente no hay nada que bloquear',
 
-    'ref_ip' => 'IP Address',
+    'ref_ip' => 'Dirección IP',
 
-    'ref_user_agent' => 'User Agent',
+    'ref_user_agent' => 'Agente de usuario',
 
     'ref_url' => 'URL',
 
-    'ref_blockedlisted' => 'Listed to block',
+    'ref_blockedlisted' => 'Lista para bloquear',
 
-    'ref_no_blockedlist_table' => 'Blockedlist database table does not exist',
+    'ref_no_blockedlist_table' => 'La tabla de base de datos bloqueada no existe',
 
 );
 
