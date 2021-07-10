7bd98adc01e5d691a91c48cfd5237ac72e19554c
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:59:26 +0300

    New translations comment_lang.php (Spanish)

--- system/ee/language/Spanish/comment_lang.php
+++ system/ee/language/Spanish/comment_lang.php
@@ -2,201 +2,201 @@
 
 $lang = array(
 
-    'already_subscribed' => 'You are already subscribed to this comment.',
+    'already_subscribed' => 'Ya estás suscrito a este comentario.',
 
-    'any_date' => 'Custom Date Range',
+    'any_date' => 'Rango de fecha personalizado',
 
-    'block_ips' => 'Add IPs to Blocked List',
+    'block_ips' => 'Añadir IP a la lista bloqueada',
 
-    'choose_entry_for_comment_move' => 'Choose Entry for Comment Move',
+    'choose_entry_for_comment_move' => 'Elegir entrada para mover comentarios',
 
-    'close_selected' => 'Set Status to Closed',
+    'close_selected' => 'Establecer estado a cerrado',
 
-    'closed' => 'Closed',
+    'closed' => 'Cerrado',
 
-    'cmt_account_not_active' => 'Your membership account has not been activated yet',
+    'cmt_account_not_active' => 'Su cuenta de miembro aún no ha sido activada',
 
-    'cmt_banned_email' => 'The email address you submitted is banned',
+    'cmt_banned_email' => 'La dirección de correo electrónico que has enviado está bloqueada',
 
-    'cmt_comment_accepted' => 'Comment Accepted',
+    'cmt_comment_accepted' => 'Comentario aceptado',
 
-    'cmt_comment_removal' => 'To stop receiving notifications for this comment, click here:',
+    'cmt_comment_removal' => 'Para dejar de recibir notificaciones para este comentario, haga clic aquí:',
 
-    'cmt_comment_response_body' => 'Someone just responded to the entry you subscribed to at:',
+    'cmt_comment_response_body' => 'Alguien acaba de responder a la entrada a la que te has suscrito en:',
 
-    'cmt_comment_response_title' => 'Someone just responded to your comment',
+    'cmt_comment_response_title' => 'Alguien acaba de responder a tu comentario',
 
-    'cmt_comment_title' => 'The title of the entry is:',
+    'cmt_comment_title' => 'El título de la entrada es:',
 
-    'cmt_comment_url' => 'You can see the comment at the following URL:',
+    'cmt_comment_url' => 'Puedes ver el comentario en la siguiente URL:',
 
-    'cmt_commenting_has_expired' => 'Commenting is not available in this channel entry.',
+    'cmt_commenting_has_expired' => 'Comentarios no están disponibles en esta entrada del canal.',
 
-    'cmt_comments_not_allowed' => 'Comments are not allowed in this channel',
+    'cmt_comments_not_allowed' => 'Los comentarios no están permitidos en este canal',
 
-    'cmt_comments_timelock' => 'You are only allowed to post comments every %s seconds.',
+    'cmt_comments_timelock' => 'Sólo puedes publicar comentarios cada %s segundos.',
 
-    'cmt_duplicate_comment_warning' => 'Unable to receive your comment at this time.',
+    'cmt_duplicate_comment_warning' => 'No se puede recibir tu comentario en este momento.',
 
-    'cmt_invalid_email' => 'The email address you submitted is not valid',
+    'cmt_invalid_email' => 'La dirección de correo electrónico que ha enviado no es válida',
 
-    'cmt_missing_comment' => 'The comment field is required',
+    'cmt_missing_comment' => 'El campo de comentario es obligatorio',
 
-    'cmt_missing_email' => 'The email field is required',
+    'cmt_missing_email' => 'El campo email es obligatorio',
 
-    'cmt_missing_name' => 'The name field is required',
+    'cmt_missing_name' => 'El campo nombre es obligatorio',
 
-    'cmt_must_be_member' => 'You must be a registered member to post comments',
+    'cmt_must_be_member' => 'Debes ser un miembro registrado para publicar comentarios',
 
-    'cmt_name_not_allowed' => 'The name you are posting with is reserved. Please choose a different name.',
+    'cmt_name_not_allowed' => 'El nombre con el que estás publicando está reservado. Por favor, elige un nombre diferente.',
 
-    'cmt_no_authorized_for_comments' => 'You are not allowed to post comments',
+    'cmt_no_authorized_for_comments' => 'No tienes permisos para publicar comentarios',
 
-    'cmt_no_preview_template_specified' => 'The comment form tag does not contain the location of your preview template.',
+    'cmt_no_preview_template_specified' => 'La etiqueta de formulario de comentario no contiene la ubicación de su plantilla de vista previa.',
 
-    'cmt_notification_removal' => 'Notification removal',
+    'cmt_notification_removal' => 'Eliminar notificaciones',
 
-    'cmt_return_to_comments' => 'Return to Comments',
+    'cmt_return_to_comments' => 'Volver a los comentarios',
 
-    'cmt_subscribe' => 'Subscribe to this comment',
+    'cmt_subscribe' => 'Suscribirse a este comentario',
 
-    'cmt_must_be_logged_in' => 'This page is only accessible to logged-in users.',
+    'cmt_must_be_logged_in' => 'Esta página sólo es accesible para los usuarios conectados.',
 
-    'cmt_too_large' => 'The comment you submitted contains %n characters. Only %x characters are allowed.',
+    'cmt_too_large' => 'El comentario enviado contiene %n caracteres. Sólo se permiten %x caracteres.',
 
-    'cmt_unsubscribe' => 'Un-subscribe from this comment',
+    'cmt_unsubscribe' => 'Desuscribirse de este comentario',
 
-    'cmt_will_be_reviewed' => 'Your comment will be reviewed by a moderator for approval.',
+    'cmt_will_be_reviewed' => 'Su comentario será revisado por un moderador para su aprobación.',
 
-    'cmt_you_have_been_removed' => 'You have been removed from receiving further notifications.',
+    'cmt_you_have_been_removed' => 'Has sido eliminado de recibir más notificaciones.',
 
-    'collapsed_display' => 'Collapsed Display',
+    'collapsed_display' => 'Pantalla colapsada',
 
-    'comment' => 'Comment',
+    'comment' => 'Comentario',
 
-    'comment_deleted' => 'Comment Deleted',
+    'comment_deleted' => 'Comentario eliminado',
 
-    'comment_edit_time_limit' => 'Comment Edit Time Limit (in seconds, set to 0 for no limit)',
+    'comment_edit_time_limit' => 'Comentar editar límite de tiempo (en segundos, fijar a 0 para sin límite)',
 
-    'comment_moderation_override' => 'Moderate expired entries',
+    'comment_moderation_override' => 'Moderar entradas caducadas',
 
-    'comment_module_description' => 'User commenting system',
+    'comment_module_description' => 'Sistema de comentarios del usuario',
 
-    'comment_module_name' => 'Comment',
+    'comment_module_name' => 'Comentario',
 
-    'comment_moved' => 'Comment moved',
+    'comment_moved' => 'Comentario movido',
 
-    'comment_settings' => 'Comment Settings',
+    'comment_settings' => 'Ajustes del comentario',
 
-    'comment_smart_notifications' => 'Enable Smart Notifications',
+    'comment_smart_notifications' => 'Habilitar notificaciones inteligentes',
 
-    'comment_updated' => 'Comment Updated',
+    'comment_updated' => 'Comentario actualizado',
 
-    'comment_word_censoring' => 'Force word censoring for comments',
+    'comment_word_censoring' => 'Forzar censura de palabras para comentarios',
 
-    'comments' => 'Comments',
+    'comments' => 'Comentarios',
 
-    'comments_moved' => 'Comments moved',
+    'comments_moved' => 'Comentarios movidos',
 
     'date_range' => 'Date Range',
 
-    'delete_confirm' => 'Delete Comment Confirmation',
+    'delete_confirm' => 'Confirmación de borrado de comentario',
 
-    'delete_selected' => 'Delete Selected',
+    'delete_selected' => 'Eliminar seleccionados',
 
-    'edit_comment' => 'Edit Comment',
+    'edit_comment' => 'Editar comentario',
 
-    'email' => 'Email',
+    'email' => 'E-mail',
 
-    'end_date' => 'End Date',
+    'end_date' => 'Fecha de fin',
 
-    'entry_title' => 'Entry Title',
+    'entry_title' => 'Título de entrada',
 
-    'expand' => 'Expand',
+    'expand' => 'Expandir',
 
-    'expanded_display' => 'Expanded Display',
+    'expanded_display' => 'Pantalla expandida',
 
-    'filter_by_author' => 'Filter by Author',
+    'filter_by_author' => 'Filtrar por autor',
 
-    'filter_by_channel' => 'Filter by Channel',
+    'filter_by_channel' => 'Filtrar por canal',
 
-    'filter_by_status' => 'Filter by Status',
+    'filter_by_status' => 'Filtrar por Estado',
 
-    'filter_comments' => 'Filter Comments',
+    'filter_comments' => 'Filtrar comentarios',
 
-    'instructions' => 'Instructions: ',
+    'instructions' => 'Instrucciones: ',
 
-    'invalid_entry_id' => 'Invalid Entry ID',
+    'invalid_entry_id' => 'ID de entrada inválida',
 
-    'invalid_subscription' => 'The URL you submitted is invalid',
+    'invalid_subscription' => 'La URL que has enviado no es válida',
 
     'ip_address' => 'IP',
 
-    'keywords' => 'Keywords',
+    'keywords' => 'Palabras clave',
 
-    'location' => 'Location',
+    'location' => 'Ubicación',
 
-    'move' => 'Move',
+    'move' => 'Mover',
 
-    'move_comment_instr' => 'Enter the new entry id number:',
+    'move_comment_instr' => 'Introduzca el id de la nueva entrada:',
 
-    'move_comments_to_entry' => 'Move Comments to Entry',
+    'move_comments_to_entry' => 'Mover comentarios a la entrada',
 
-    'move_selected' => 'Move Selected',
+    'move_selected' => 'Mover seleccionados',
 
-    'name' => 'Name',
+    'name' => 'Nombre',
 
-    'no_results' => 'No Results',
+    'no_results' => 'Sin resultados',
 
-    'no_valid_comments' => 'No Valid Comments',
+    'no_valid_comments' => 'No hay comentarios válidos',
 
-    'not_currently_subscribed' => 'You do not appear to be subscribed to the entry ID you submitted',
+    'not_currently_subscribed' => 'No parece estar suscrito al ID de la entrada que ha enviado',
 
-    'open' => 'Open',
+    'open' => 'Abrir',
 
-    'open_selected' => 'Set Status to Open',
+    'open_selected' => 'Establecer estado a abrir',
 
-    'past_day' => 'Last 24 hours',
+    'past_day' => 'Últimas 24 horas',
 
-    'past_month' => 'Last 30 days',
+    'past_month' => 'Últimos 30 días',
 
-    'past_six_months' => 'Last 180 days',
+    'past_six_months' => 'Últimos 180 días',
 
-    'past_week' => 'Last 7 days',
+    'past_week' => 'Últimos 7 días',
 
-    'past_year' => 'Last 365 days',
+    'past_year' => 'Últimos 365 días',
 
-    'pending_selected' => 'Set Status to Pending',
+    'pending_selected' => 'Establecer estado a Pendiente',
 
-    'registered_member' => 'Registered member',
+    'registered_member' => 'Miembro registrado',
 
-    'search_in_comments' => 'Search in Comments',
+    'search_in_comments' => 'Buscar en Comentarios',
 
-    'search_in_emails' => 'Search in Email Addresses',
+    'search_in_emails' => 'Buscar en direcciones de correo electrónico',
 
-    'search_in_entry_titles' => 'Search in Entry Titles',
+    'search_in_entry_titles' => 'Buscar en los títulos de entrada',
 
-    'search_in_ips' => 'Search in IPs',
+    'search_in_ips' => 'Buscar en IMs',
 
-    'search_in_names' => 'Search in Names',
+    'search_in_names' => 'Buscar en nombres',
 
-    'selection_required' => 'At least one selection is required to submit',
+    'selection_required' => 'Se requiere al menos una selección para enviar',
 
-    'settings' => 'Settings',
+    'settings' => 'Ajustes',
 
-    'settings_updated' => 'Settings Updated',
+    'settings_updated' => 'Configuración actualizada',
 
-    'start_date' => 'Start Date',
+    'start_date' => 'Fecha de inicio',
 
-    'status_changed' => 'Status Changed',
+    'status_changed' => 'Estado cambiado',
 
-    'today' => 'Today',
+    'today' => 'Hoy',
 
     'url' => 'URL',
 
-    'you_have_been_subscribed' => 'You are now subscribed to the following entry:',
+    'you_have_been_subscribed' => 'Ahora estás suscrito a la siguiente entrada:',
 
-    'you_have_been_unsubscribed' => 'Your subscription to the following entry has been removed:',
+    'you_have_been_unsubscribed' => 'Su suscripción a la siguiente entrada ha sido eliminada:',
 
 );
 
