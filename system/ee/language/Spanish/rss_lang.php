2168691a1f2727213d986ad45c7592634a8d3f70
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:58:58 +0300

    New translations rss_lang.php (Spanish)

--- system/ee/language/Spanish/rss_lang.php
+++ system/ee/language/Spanish/rss_lang.php
@@ -2,9 +2,9 @@
 
 $lang = array(
 
-    'rss_invalid_channel' => 'The channel specified in your RSS feed does not exist.',
+    'rss_invalid_channel' => 'El canal especificado en su feed RSS no existe.',
 
-    'rss_module_description' => 'RSS page generating module',
+    'rss_module_description' => 'Generando módulo de página RSS',
 
     'rss_module_name' => 'RSS',
 
