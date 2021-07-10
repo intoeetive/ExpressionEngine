1815e0b1aa52dc6108badf1dd797cb77818d2747
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:57:56 +0300

    New translations ip_to_nation_lang.php (Spanish)

--- system/ee/language/Spanish/ip_to_nation_lang.php
+++ system/ee/language/Spanish/ip_to_nation_lang.php
@@ -2,67 +2,67 @@
 
 $lang = array(
 
-    'ban_info' => 'Select the countries you wish to ban. When a country is banned, a person with a IP address corresponding to it will not be able to submit comments, or use your email/tell-a-friend form. They will still be able to view your site.',
+    'ban_info' => 'Seleccione los países que desea banear. Cuando un país está prohibido, una persona con una dirección IP correspondiente no podrá enviar comentarios, o utilice su formulario de correo electrónico/cont-a-friend. Todavía podrán ver su sitio.',
 
-    'banlist' => 'Nation Banlist',
+    'banlist' => 'Lista de Baneos de Nación',
 
-    'banlist_updated' => 'Banlist Updated',
+    'banlist_updated' => 'Lista prohibida actualizada',
 
-    'banlist_updated_desc' => 'The banlist has been successfully updated.',
+    'banlist_updated_desc' => 'La lista de baneos se ha actualizado correctamente.',
 
-    'banned_countries' => 'Banned countries',
+    'banned_countries' => 'Países prohibidos',
 
-    'btn_save_banlist' => 'Save Banlist',
+    'btn_save_banlist' => 'Guardar Banlist',
 
-    'btn_search' => 'Search',
+    'btn_search' => 'Buscar',
 
-    'btn_searching' => 'Searching...',
+    'btn_searching' => 'Buscando...',
 
-    'cache_full' => 'Cache is full and must be cleared manually to proceed.',
+    'cache_full' => 'La caché está llena y debe ser borrada manualmente para continuar.',
 
-    'countryfile_missing' => 'Unable to find the file called country.php in your config folder.',
+    'countryfile_missing' => 'No se puede encontrar el archivo llamado country.php en la carpeta de configuración.',
 
-    'ip_address_located' => 'IP Address Located',
+    'ip_address_located' => 'Dirección IP localizada',
 
-    'ip_address_not_located' => 'Unable to Locate IP Address',
+    'ip_address_not_located' => 'No se puede localizar la dirección IP',
 
-    'ip_db_connection_403' => '403 Connection Error, please wait 24 hours before attempting to retrieve database.',
+    'ip_db_connection_403' => 'Error de conexión 403, por favor espere 24 horas antes de intentar recuperar la base de datos.',
 
-    'ip_db_connection_error' => 'Unable to connect to data provider.',
+    'ip_db_connection_error' => 'No se puede conectar al proveedor de datos.',
 
-    'ip_db_downloaded' => 'Downloaded IP database. Unpacking ...',
+    'ip_db_downloaded' => 'Base de datos IP descargada. Desempaquetando...',
 
-    'ip_db_failed' => 'IP databse update failed',
+    'ip_db_failed' => 'Actualización de base de datos IP fallida',
 
-    'ip_db_unpacked' => 'Unpacked IP database. Inserting data ...',
+    'ip_db_unpacked' => 'Base de datos IP desempaquetada. Insertando datos...',
 
-    'ip_db_updated' => 'IP database update successful',
+    'ip_db_updated' => 'Base de datos IP actualizada correctamente',
 
-    'ip_db_updating' => 'Downloading update ...',
+    'ip_db_updating' => 'Descargando actualización...',
 
-    'ip_not_found' => 'There are no results for the IP you submitted.',
+    'ip_not_found' => 'No hay resultados para la IP que ha enviado.',
 
-    'ip_not_valid' => 'The IP you submitted is not a valid IP address.',
+    'ip_not_valid' => 'La IP que ha enviado no es una dirección IP válida.',
 
-    'ip_result' => 'The IP you submitted is from the following country:',
+    'ip_result' => 'La IP que has enviado es del siguiente país:',
 
-    'ip_search' => 'IP Address Search',
+    'ip_search' => 'Búsqueda de dirección IP',
 
-    'ip_to_nation_module_name' => 'IP to Nation',
+    'ip_to_nation_module_name' => 'IP a la nación',
 
-    'last_update' => 'Last update: ',
+    'last_update' => 'Última actualización: ',
 
-    'manage_banlist' => 'Manage Your Banned Country List',
+    'manage_banlist' => 'Administra tu lista de países prohibidos',
 
-    'no_countries' => 'No <b>Countries</b> found.',
+    'no_countries' => 'No se encontraron <b>Países</b>.',
 
-    'search_for_ip' => 'Search for an IP address',
+    'search_for_ip' => 'Buscar una dirección IP',
 
-    'search_for_ip_desc' => 'Enter an IP address to determine the country it is associated with.',
+    'search_for_ip_desc' => 'Introduzca una dirección IP para determinar el país con el que está asociado.',
 
-    'update_info' => 'Updates the IP and country code information. Data source: <a href="%s" title="Data Provider" rel="external">maxmind.com</a>.</em><em><i>The new data will automatically be downloaded and applied. This may take several minutes and increase load on your database.</i></em>',
+    'update_info' => 'Actualiza la dirección IP y el código del país. Fuente de datos: <a href="%s" title="Data Provider" rel="external">maxmind.com</a>.</em><em><i>Los nuevos datos se descargarán y aplicarán automáticamente. Esto puede tardar varios minutos y aumentar la carga en su base de datos.</i></em>',
 
-    'update_ips' => 'Update IP database',
+    'update_ips' => 'Actualizar base de datos IP',
 
 );
 
