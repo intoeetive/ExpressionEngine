19217778a4935bbf474a5eb395017500a421746e
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:58:56 +0300

    New translations query_lang.php (Spanish)

--- system/ee/language/Spanish/query_lang.php
+++ system/ee/language/Spanish/query_lang.php
@@ -2,9 +2,9 @@
 
 $lang = array(
 
-    'query_module_description' => 'SQL query module for templates',
+    'query_module_description' => 'Módulo de consulta SQL para plantillas',
 
-    'query_module_name' => 'Query',
+    'query_module_name' => 'Consulta',
 
 );
 
