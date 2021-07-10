e0ab4f2c182c58c89b687c2edd6799d8c37442bb
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:58:17 +0300

    New translations fieldtypes_lang.php (Spanish)

--- system/ee/language/Spanish/fieldtypes_lang.php
+++ system/ee/language/Spanish/fieldtypes_lang.php
@@ -2,74 +2,74 @@
 
 $lang = array(
 
-    'add_file' => 'Add File',
+    'add_file' => 'Añadir archivo',
 
-    'directory' => 'Directory:',
+    'directory' => 'Directorio:',
 
-    'directory_no_access' => 'You do not have access to the directory specified for this field',
+    'directory_no_access' => 'No tiene acceso al directorio especificado para este campo',
 
-    'file_undo_remove' => 'Undo Remove',
+    'file_undo_remove' => 'Deshacer Eliminar',
 
-    'remove_file' => 'Remove File',
+    'remove_file' => 'Quitar archivo',
 
     /* Relationships */
-    'any_author' => 'Any author',
+    'any_author' => 'Cualquier autor',
 
-    'any_category' => 'Any category',
+    'any_category' => 'Cualquier categoría',
 
-    'any_channel' => 'Any channel',
+    'any_channel' => 'Cualquier canal',
 
-    'any_status' => 'Any status',
+    'any_status' => 'Cualquier estado',
 
-    'relate_entry' => 'Relate Entry',
+    'relate_entry' => 'Relacionar entrada',
 
-    'rel_ft_allow_multi' => 'Allow multiple relationships?',
+    'rel_ft_allow_multi' => '¿Permitir múltiples relaciones?',
 
-    'rel_ft_allow_multi_desc' => 'When enabled, authors will be allowed to create multiple relationships.',
+    'rel_ft_allow_multi_desc' => 'Cuando está habilitado, se permitirá a los autores crear múltiples relaciones.',
 
-    'rel_ft_authors' => 'Authors',
+    'rel_ft_authors' => 'Autores',
 
-    'rel_ft_authors_desc' => 'Choose authors to limit the entries in this relationships field.',
+    'rel_ft_authors_desc' => 'Seleccione los autores para limitar las entradas en este campo de relaciones.',
 
-    'rel_ft_categories' => 'Limit by category',
+    'rel_ft_categories' => 'Limitar por categoría',
 
-    'rel_ft_categories_desc' => 'Choose categories to limit the entries in this relationships field.',
+    'rel_ft_categories_desc' => 'Elija categorías para limitar las entradas en este campo de relaciones.',
 
-    'rel_ft_channels' => 'Channels to relate',
+    'rel_ft_channels' => 'Canales a relacionar',
 
-    'rel_ft_channels_desc' => 'Which channels can related content be pulled from.',
+    'rel_ft_channels_desc' => 'De qué canales se puede extraer el contenido relacionado.',
 
-    'rel_ft_include' => 'Include in selection',
+    'rel_ft_include' => 'Incluye en la selección',
 
-    'rel_ft_include_desc' => 'Allow expired or future entries in this relationships field.',
+    'rel_ft_include_desc' => 'Permitir entradas caducadas o futuras en este campo de relaciones.',
 
-    'rel_ft_include_expired' => 'Expired entries',
+    'rel_ft_include_expired' => 'Entradas caducadas',
 
-    'rel_ft_include_future' => 'Future entries',
+    'rel_ft_include_future' => 'Entradas futuras',
 
-    'rel_ft_limit' => 'Maximum entries',
+    'rel_ft_limit' => 'Máximo de entradas',
 
-    'rel_ft_limit_desc' => 'Maximum number of entries to show in relationship field.<br><i>Leave blank to allow all entries.</i>',
+    'rel_ft_limit_desc' => 'Número máximo de entradas para mostrar en el campo de relación.<br><i>Dejar en blanco para permitir todas las entradas.</i>',
 
-    'rel_ft_order' => 'Order by',
+    'rel_ft_order' => 'Ordenar por',
 
-    'rel_ft_order_ascending' => 'Ascending (A-Z)',
+    'rel_ft_order_ascending' => 'Ascendente (A-Z)',
 
-    'rel_ft_order_date' => 'Entry Date',
+    'rel_ft_order_date' => 'Fecha de entrada',
 
-    'rel_ft_order_desc' => 'Default ordering of entries in relationship field.',
+    'rel_ft_order_desc' => 'Orden por defecto de las entradas en el campo de la relación.',
 
-    'rel_ft_order_descending' => 'Descending (Z-A)',
+    'rel_ft_order_descending' => 'Descendente (Z-A)',
 
-    'rel_ft_order_title' => 'Title',
+    'rel_ft_order_title' => 'Título',
 
-    'rel_ft_related_only' => 'Related only',
+    'rel_ft_related_only' => 'Solo relacionados',
 
-    'rel_ft_statuses' => 'Limit by status',
+    'rel_ft_statuses' => 'Limitar por estado',
 
-    'rel_ft_statuses_desc' => 'Choose statuses to limit the entries in this relationships field.',
+    'rel_ft_statuses_desc' => 'Elija los estados para limitar las entradas en este campo de relaciones.',
 
-    'rel_ft_unrelated_only' => 'Unrelated only',
+    'rel_ft_unrelated_only' => 'Sólo no relacionados',
 
     /* Duration */
     'duration_ft_hh' => 'hh',
@@ -78,201 +78,201 @@ $lang = array(
 
     'duration_ft_hhmmss' => 'hh:mm:ss',
 
-    'duration_ft_hours' => 'Hours',
+    'duration_ft_hours' => 'Horas',
 
-    'duration_ft_minutes' => 'Minutes',
+    'duration_ft_minutes' => 'Minutos',
 
-    'duration_ft_placeholder' => 'Duration in %s (or %s)',
+    'duration_ft_placeholder' => 'Duración en %s (o %s)',
 
-    'duration_ft_seconds' => 'Seconds',
+    'duration_ft_seconds' => 'Segundos',
 
-    'duration_ft_units' => 'Units',
+    'duration_ft_units' => 'Unidades',
 
-    'duration_ft_units_desc' => 'Determines the length of time being described when a content author enters a whole number like “42”.',
+    'duration_ft_units_desc' => 'Determina la duración del tiempo que se describe cuando un autor de contenido introduce un número entero como “42”.',
 
     /* File */
-    'file_ft_allowed_dirs' => 'Allowed Directory',
+    'file_ft_allowed_dirs' => 'Directorio permitido',
 
-    'file_ft_allowed_dirs_desc' => 'Default directory to store files uploaded with this field.',
+    'file_ft_allowed_dirs_desc' => 'Directorio predeterminado para almacenar archivos subidos con este campo.',
 
-    'file_ft_cannot_find_file' => '<b>File not found.</b> We could not locate %s on the server.',
+    'file_ft_cannot_find_file' => '<b>Archivo no encontrado.</b> No pudimos localizar %s en el servidor.',
 
-    'file_ft_configure' => 'General Field Options',
+    'file_ft_configure' => 'Opciones generales de campo',
 
-    'file_ft_configure_subtext' => 'Further control how the file field should behave.',
+    'file_ft_configure_subtext' => 'Control adicional de cómo debe comportarse el campo del archivo.',
 
-    'file_ft_content_type' => 'Allowed file types',
+    'file_ft_content_type' => 'Tipos de archivo permitidos',
 
-    'file_ft_content_type_desc' => 'Types of files that can be uploaded from this field.',
+    'file_ft_content_type_desc' => 'Tipos de archivos que pueden ser subidos desde este campo.',
 
-    'file_ft_images_only' => 'Images Only',
+    'file_ft_images_only' => 'Solo imágenes',
 
-    'file_ft_limit' => 'Existing files limit',
+    'file_ft_limit' => 'Límite de archivos existentes',
 
-    'file_ft_limit_desc' => 'Maximum number of files to show in the drop down.<br><i>Leave blank to show all files.</i>',
+    'file_ft_limit_desc' => 'Número máximo de archivos a mostrar en el menú desplegable.<br><i>Dejar en blanco para mostrar todos los archivos.</i>',
 
-    'file_ft_no_upload_directories' => 'There are currently no upload directories available. Please <a href="%s">add one or more upload directories</a> to use the File field type.',
+    'file_ft_no_upload_directories' => 'Actualmente no hay directorios de subida disponibles. Por favor, <a href="%s">agregue uno o más directorios de subida</a> para utilizar el tipo de campo de archivo.',
 
-    'file_ft_select_existing' => 'Select an existing file',
+    'file_ft_select_existing' => 'Seleccione un archivo existente',
 
-    'file_ft_show_files' => 'Show existing files?',
+    'file_ft_show_files' => '¿Mostrar archivos existentes?',
 
-    'file_ft_show_files_desc' => 'When enabled, a drop down with existing files will be shown to authors.',
+    'file_ft_show_files_desc' => 'Cuando está habilitado, se mostrará un desplegable con archivos existentes a los autores.',
 
-    'file_ft_upload_directories' => 'Upload Directories',
+    'file_ft_upload_directories' => 'Subir Directorios',
 
     /* File Drag and Drop */
 
-    'file_dnd_choose_directory' => 'Please choose a directory',
+    'file_dnd_choose_directory' => 'Por favor, elija un directorio',
 
-    'file_dnd_choose_file_directory' => 'Choose a File Directory',
+    'file_dnd_choose_file_directory' => 'Elegir un directorio de archivos',
 
-    'file_dnd_upload_to' => 'Upload To',
+    'file_dnd_upload_to' => 'Subir a',
 
-    'file_dnd_choose_directory_before_uploading' => 'You must choose a directory to upload files',
+    'file_dnd_choose_directory_before_uploading' => 'Debes elegir un directorio para subir archivos',
 
-    'file_dnd_choose_directory_btn' => 'Choose Directory',
+    'file_dnd_choose_directory_btn' => 'Elegir directorio',
 
-    'file_dnd_choose_existing' => 'Choose Existing',
+    'file_dnd_choose_existing' => 'Elegir existente',
 
-    'file_dnd_dismiss' => 'Dismiss',
+    'file_dnd_dismiss' => 'Descartar',
 
-    'file_dnd_drop_file' => 'Drop File Here to Upload',
+    'file_dnd_drop_file' => 'Borrar archivo aquí para subir',
 
-    'file_dnd_drop_files' => 'Drop File(s) Here to Upload',
+    'file_dnd_drop_files' => 'Borrar archivo(s) aquí para subir',
 
-    'file_dnd_file_name' => 'File Name',
+    'file_dnd_file_name' => 'Nombre del archivo',
 
-    'file_dnd_filter_directories' => 'filter directories',
+    'file_dnd_filter_directories' => 'directorios de filtros',
 
-    'file_dnd_images_only' => 'This field only accepts images.',
+    'file_dnd_images_only' => 'Este campo sólo acepta imágenes.',
 
-    'file_dnd_progress' => 'Progress',
+    'file_dnd_progress' => 'Progreso',
 
-    'file_dnd_conflict' => 'Error. File already exists.',
+    'file_dnd_conflict' => 'Error. El archivo ya existe.',
 
-    'file_dnd_resolve_conflict' => 'Resolve Conflict',
+    'file_dnd_resolve_conflict' => 'Resolver conflicto',
 
-    'file_dnd_single_file_allowed' => 'This field can only contain one file',
+    'file_dnd_single_file_allowed' => 'Este campo sólo puede contener un archivo',
 
-    'file_dnd_unexpected_error' => 'Unexpected error. Check your post_max_size setting in your PHP configuration.',
+    'file_dnd_unexpected_error' => 'Error inesperado. Compruebe su configuración post_max_size en su configuración PHP.',
 
-    'file_dnd_uploading_to' => 'uploading to %s',
+    'file_dnd_uploading_to' => 'subiendo a %s',
 
-    'file_dnd_upload_new' => 'Upload New',
+    'file_dnd_upload_new' => 'Subir nuevo',
 
     /* Grid */
-    'grid_add_field' => 'add new field',
+    'grid_add_field' => 'añadir nuevo campo',
 
-    'grid_add_some_data' => 'You have not added any rows of data yet. <a href="#" class="grid_link_add">Add some data?</a>',
+    'grid_add_some_data' => 'Aún no has añadido ninguna fila de datos. <a href="#" class="grid_link_add">¿Añadir algunos datos?</a>',
 
-    'grid_allow_reorder' => 'Allow reordering of rows?',
+    'grid_allow_reorder' => '¿Permitir reordenar las filas?',
 
-    'grid_chars_allowed' => 'Characters allowed.',
+    'grid_chars_allowed' => 'Se permiten caracteres.',
 
-    'grid_col_instr' => 'Instructions',
+    'grid_col_instr' => 'Instrucciones',
 
-    'grid_col_label' => 'Publish label',
+    'grid_col_label' => 'Publicar etiqueta',
 
-    'grid_col_name' => 'Field name',
+    'grid_col_name' => 'Nombre del campo',
 
-    'grid_col_name_reserved' => 'This column is using a column name reserved for other template functionality.',
+    'grid_col_name_reserved' => 'Esta columna utiliza un nombre de columna reservado para otras funciones de plantilla.',
 
-    'grid_col_options' => 'Is this data',
+    'grid_col_options' => 'Son estos datos',
 
-    'grid_col_type' => 'Data type?',
+    'grid_col_type' => '¿Tipo de datos?',
 
-    'grid_col_width' => 'Column minimum width',
+    'grid_col_width' => 'Ancho mínimo de columna',
 
-    'grid_col_width_desc' => 'Set the minimum width (in pixels) of this column in the publish form.',
+    'grid_col_width_desc' => 'Establece el ancho mínimo (en píxeles) de esta columna en el formulario de publicación.',
 
-    'grid_col_width_percent' => 'Percentage.',
+    'grid_col_width_percent' => 'Porcentaje.',
 
-    'grid_config_desc' => 'What data do you want to collect?',
+    'grid_config_desc' => '¿Qué datos desea recopilar?',
 
-    'grid_copy_field' => 'copy field',
+    'grid_copy_field' => 'copiar campo',
 
-    'grid_date_localized' => 'Localized?',
+    'grid_date_localized' => '¿Localizado?',
 
-    'grid_duplicate_col_label' => 'Column field labels must be unique.',
+    'grid_duplicate_col_label' => 'Las etiquetas de campo de columna deben ser únicas.',
 
-    'grid_duplicate_col_name' => 'Column field names must be unique.',
+    'grid_duplicate_col_name' => 'Los nombres de campos de columna deben ser únicos.',
 
-    'grid_expand_field' => 'expand field',
+    'grid_expand_field' => 'ampliar campo',
 
-    'grid_field_required' => 'This field is required',
+    'grid_field_required' => 'Este campo es obligatorio',
 
-    'grid_fields' => 'Grid Fields',
+    'grid_fields' => 'Campos de cuadrícula',
 
-    'grid_in_this_field' => 'Is this field',
+    'grid_in_this_field' => 'Es este campo',
 
-    'grid_in_this_field_desc' => 'Make this field required, or searchable.',
+    'grid_in_this_field_desc' => 'Haga que este campo sea obligatorio, o que se pueda buscar.',
 
     'grid_limit_input' => 'Limit input?',
 
-    'grid_max_rows' => 'Maximum rows',
+    'grid_max_rows' => 'Máximo de filas',
 
-    'grid_max_rows_desc' => 'Sets the maximum amount of data rows this grid will accept',
+    'grid_max_rows_desc' => 'Establece la cantidad máxima de registros de datos que esta cuadrícula aceptará',
 
-    'grid_min_rows' => 'Minimum rows',
+    'grid_min_rows' => 'Filas mínimas',
 
-    'grid_min_rows_desc' => 'Sets the minumum amount of data rows this grid will accept',
+    'grid_min_rows_desc' => 'Establece la cantidad mínima de registros de datos que esta cuadrícula aceptará',
 
-    'grid_order_by' => 'Order by',
+    'grid_order_by' => 'Ordenar por',
 
-    'grid_output_format' => 'Output formatting?',
+    'grid_output_format' => '¿Formato de salida?',
 
-    'grid_remove_field' => 'remove field',
+    'grid_remove_field' => 'eliminar campo',
 
-    'grid_reorder_field' => 'reorder field',
+    'grid_reorder_field' => 'reordenar campo',
 
-    'grid_show' => 'Show',
+    'grid_show' => 'Mostrar',
 
-    'grid_show_fmt_btns' => 'Show formatting buttons?',
+    'grid_show_fmt_btns' => '¿Mostrar botones de formato?',
 
-    'grid_text_direction' => 'Text direction?',
+    'grid_text_direction' => '¿Dirección del texto?',
 
-    'grid_validation_error' => 'There was a problem with one or more Grid fields',
+    'grid_validation_error' => 'Hubo un problema con uno o más campos de cuadrícula',
 
     /* File Grid */
 
-    'file_grid_setup' => 'File Grid Setup',
+    'file_grid_setup' => 'Configuración de cuadrícula',
 
-    'file_grid_maximum_rows_hit' => 'Uploading this many files will exceed the field\'s maximum rows setting of %s',
+    'file_grid_maximum_rows_hit' => 'Subir este número de archivos excederá la configuración máxima de filas del campo de %s',
 
     /* URL */
-    'url_ft_allowed_url_schemes' => 'Allowed URL Schemes',
+    'url_ft_allowed_url_schemes' => 'Esquemas de URL permitidos',
 
-    'url_ft_invalid_url' => 'Invalid URL',
+    'url_ft_invalid_url' => 'URL inválida',
 
-    'url_ft_invalid_url_scheme' => 'Your URL must begin with a valid scheme: %s',
+    'url_ft_invalid_url_scheme' => 'Su URL debe comenzar con un esquema válido: %s',
 
     'url_ft_protocol_relative_url' => 'Protocol Relative URL',
 
-    'url_ft_url_scheme_placeholder' => 'URL Scheme Placeholder',
+    'url_ft_url_scheme_placeholder' => 'Marcador de posición del esquema de URL',
 
-    'url_ft_url_scheme_placeholder_desc' => 'Displayed as placeholder text when no URL has been submitted.',
+    'url_ft_url_scheme_placeholder_desc' => 'Se muestra como texto de marcador de posición cuando no se ha enviado ninguna URL.',
 
     /* Color Picker */
-    'colorpicker_allowed_colors' => 'Allowed Colors',
-    'colorpicker_allowed_colors_desc' => 'Choose what colors are allowed to be selected.',
+    'colorpicker_allowed_colors' => 'Colores Permitidos',
+    'colorpicker_allowed_colors_desc' => 'Elige los colores que se permiten seleccionar.',
 
-    'colorpicker_allowed_colors_any' => 'Any',
-    'colorpicker_allowed_colors_swatches' => 'Swatches',
+    'colorpicker_allowed_colors_any' => 'Cualquier',
+    'colorpicker_allowed_colors_swatches' => 'Relojes',
 
-    'colorpicker_default_color' => 'Default Color',
-    'colorpicker_default_color_desc' => 'When set, if no color is selected the default color will be used instead.',
+    'colorpicker_default_color' => 'Color por defecto',
+    'colorpicker_default_color_desc' => 'Cuando se establece, si no se selecciona ningún color, se utilizará el color predeterminado en su lugar.',
 
-    'colorpicker_color_not_allowed' => 'This color is not allowed.',
+    'colorpicker_color_not_allowed' => 'Este color no está permitido.',
 
-    'no_colorpicker_swatches' => 'No <b>Swatches</b> exist.',
+    'no_colorpicker_swatches' => 'No existe <b>Swatches</b>.',
 
-    'swatches' => 'Swatches',
+    'swatches' => 'Relojes',
 
-    'colorpicker_swatches_options_desc' => 'Specify a list of pre-defined colors to show in the color picker.',
+    'colorpicker_swatches_options_desc' => 'Especifique una lista de colores predefinidos para mostrar en el selector de colores.',
 
-    'colorpicker_swatches_populate_values' => 'Values',
-    'colorpicker_swatches_populate_manually' => 'Populate the swatches manually',
+    'colorpicker_swatches_populate_values' => 'Valores',
+    'colorpicker_swatches_populate_manually' => 'Poblar las muestras manualmente',
 
 );
 
