97383a96c3ad7f974771a58ce259c4f1ad148974
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:58:28 +0300

    New translations db_lang.php (Spanish)

--- system/ee/language/Spanish/db_lang.php
+++ system/ee/language/Spanish/db_lang.php
@@ -2,53 +2,53 @@
 
 $lang = array(
 
-    'db_batch_missing_index' => 'One or more rows submitted for batch updating is missing the specified index.',
+    'db_batch_missing_index' => 'A una o más filas enviadas para la actualización por lotes le falta el índice especificado.',
 
-    'db_column_definition_required' => 'A column definition is required for that operation.',
+    'db_column_definition_required' => 'Se requiere una definición de columna para esa operación.',
 
-    'db_column_name_required' => 'A column name is required for that operation.',
+    'db_column_name_required' => 'Se requiere un nombre de columna para esa operación.',
 
-    'db_del_must_use_where' => 'Deletes are not allowed unless they contain a "where" or "like" clause.',
+    'db_del_must_use_where' => 'Los borrados no están permitidos a menos que contengan una cláusula "where" o "like".',
 
-    'db_error_heading' => 'A Database Error Occurred',
+    'db_error_heading' => 'Se ha producido un error en la base de datos',
 
-    'db_field_param_missing' => 'To fetch fields requires the name of the table as a parameter.',
+    'db_field_param_missing' => 'Para obtener los campos requiere el nombre de la tabla como parámetro.',
 
-    'db_filepath_error' => 'Unable to write data to the file path you have submitted.',
+    'db_filepath_error' => 'No se puede escribir datos en la ruta del archivo que ha enviado.',
 
-    'db_invalid_cache_path' => 'The cache path you submitted is not valid or writable.',
+    'db_invalid_cache_path' => 'La ruta de caché que has enviado no es válida o tiene permisos de escritura.',
 
-    'db_invalid_connection_str' => 'Unable to determine the database settings based on the connection string you submitted.',
+    'db_invalid_connection_str' => 'No se puede determinar la configuración de la base de datos basándose en la cadena de conexión que envió.',
 
-    'db_invalid_query' => 'The query you submitted is not valid.',
+    'db_invalid_query' => 'La consulta enviada no es válida.',
 
-    'db_must_set_table' => 'You must set the database table to be used with your query.',
+    'db_must_set_table' => 'Debe establecer la tabla de la base de datos para ser usada con su consulta.',
 
-    'db_must_use_index' => 'You must specify an index to match on for batch updates.',
+    'db_must_use_index' => 'Debe especificar un índice en el que coincidir para las actualizaciones por lotes.',
 
-    'db_must_use_set' => 'You must use the "set" method to update an entry.',
+    'db_must_use_set' => 'Debe utilizar el método "set" para actualizar una entrada.',
 
-    'db_must_use_where' => 'Updates are not allowed unless they contain a "where" clause.',
+    'db_must_use_where' => 'No se permiten actualizaciones a menos que contengan una cláusula "where".',
 
-    'db_table_name_required' => 'A table name is required for that operation.',
+    'db_table_name_required' => 'Se requiere un nombre de tabla para esa operación.',
 
-    'db_transaction_failure' => 'Transaction failure: Rollback performed.',
+    'db_transaction_failure' => 'Fallo en la transacción: Rollback realizado.',
 
-    'db_unable_to_connect' => 'Unable to connect to your database server using the provided settings.',
+    'db_unable_to_connect' => 'No se puede conectar al servidor de base de datos usando la configuración proporcionada.',
 
-    'db_unable_to_create' => 'Unable to create the specified database: %s',
+    'db_unable_to_create' => 'No se puede crear la base de datos especificada: %s',
 
-    'db_unable_to_drop' => 'Unable to drop the specified database.',
+    'db_unable_to_drop' => 'No se puede eliminar la base de datos especificada.',
 
-    'db_unable_to_select' => 'Unable to select the specified database: %s',
+    'db_unable_to_select' => 'No se puede seleccionar la base de datos especificada: %s',
 
-    'db_unable_to_set_charset' => 'Unable to set client connection character set: %s',
+    'db_unable_to_set_charset' => 'No se puede establecer el conjunto de caracteres de conexión del cliente: %s',
 
-    'db_unsuported_compression' => 'The file compression format you chose is not supported by your server.',
+    'db_unsuported_compression' => 'El formato de compresión de archivos que eligió no está soportado por su servidor.',
 
-    'db_unsuported_feature' => 'Unsupported feature of the database platform you are using.',
+    'db_unsuported_feature' => 'Característica no soportada de la plataforma de base de datos que está usando.',
 
-    'db_unsupported_function' => 'This feature is not available for the database you are using.',
+    'db_unsupported_function' => 'Esta característica no está disponible para la base de datos que está usando.',
 
 );
 
