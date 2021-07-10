6336a2109ff9b99bff1b990e1380bc1d2c12dd21
Yuri Salimovskiy <yuri.salimovskiy@gmail.com>
2021-07-10 14:59:01 +0300

    New translations search_lang.php (Spanish)

--- system/ee/language/Spanish/search_lang.php
+++ system/ee/language/Spanish/search_lang.php
@@ -2,155 +2,155 @@
 
 $lang = array(
 
-    'any_category' => 'Any Category',
+    'any_category' => 'Cualquier categoría',
 
-    'any_field' => 'Any Field',
+    'any_field' => 'Cualquier campo',
 
-    'categories' => 'Categories',
+    'categories' => 'Categorías',
 
-    'date_since' => 'Since',
+    'date_since' => 'Desde',
 
-    'none_of_these_words' => 'None of these words',
+    'none_of_these_words' => 'Ninguna de estas palabras',
 
-    'past_month' => 'Past Month',
+    'past_month' => 'Último mes',
 
-    'past_six_months' => 'Past Six Months',
+    'past_six_months' => 'Últimos seis meses',
 
-    'past_week' => 'Past Week',
+    'past_week' => 'Última semana',
 
-    'past_year' => 'Past Year',
+    'past_year' => 'Último año',
 
-    'rebuild_search_index' => 'Rebuild Search Index',
+    'rebuild_search_index' => 'Reconstruir índice de búsqueda',
 
-    'search' => 'Search',
+    'search' => 'Buscar',
 
-    'search_all_groups' => 'Search All Member Groups',
+    'search_all_groups' => 'Buscar todos los grupos',
 
-    'search_all_words' => 'Search for All Words',
+    'search_all_words' => 'Buscar todas las palabras',
 
-    'search_also_search_comments' => 'Also search in comments',
+    'search_also_search_comments' => 'Buscar también en comentarios',
 
-    'search_any_category' => 'Any Category',
+    'search_any_category' => 'Cualquier categoría',
 
-    'search_any_channel' => 'Any Channel',
+    'search_any_channel' => 'Cualquier canal',
 
-    'search_any_date' => 'Any date',
+    'search_any_date' => 'Cualquier fecha',
 
-    'search_any_words' => 'Search for Any Word',
+    'search_any_words' => 'Buscar cualquier palabra',
 
-    'search_ascending' => 'Ascending',
+    'search_ascending' => 'Ascendente',
 
-    'search_author' => 'Author',
+    'search_author' => 'Autor',
 
-    'search_by_keyword' => 'Search by Keyword',
+    'search_by_keyword' => 'Buscar por palabra clave',
 
-    'search_by_member_group' => 'Search by Member Group',
+    'search_by_member_group' => 'Buscar por grupo de miembros',
 
-    'search_by_member_name' => 'Search by Member Name',
+    'search_by_member_name' => 'Buscar por nombre de miembro',
 
-    'search_categories' => 'Categories',
+    'search_categories' => 'Categorías',
 
-    'search_channel' => 'Channel',
+    'search_channel' => 'Canal',
 
-    'search_channels' => 'Channels',
+    'search_channels' => 'Canales',
 
-    'search_content_from' => 'Search Content From',
+    'search_content_from' => 'Buscar contenido de',
 
-    'search_date' => 'Date',
+    'search_date' => 'Fecha',
 
-    'search_descending' => 'Descending',
+    'search_descending' => 'Descendente',
 
-    'search_engine' => 'Search Engine',
+    'search_engine' => 'Motor de búsqueda',
 
-    'search_entries' => 'Search in Titles and Entries',
+    'search_entries' => 'Buscar en títulos y asientos',
 
-    'search_entries_from' => 'Search Entries From',
+    'search_entries_from' => 'Buscar entradas de',
 
-    'search_everywhere' => 'Search in Titles, Entries and Comments',
+    'search_everywhere' => 'Buscar en Títulos, Entradas y Comentarios',
 
-    'search_exact_name_match' => 'Exact Name Match',
+    'search_exact_name_match' => 'Nombre exacto coincidencia',
 
-    'search_exact_phrase_match' => 'Exact Phrase Match',
+    'search_exact_phrase_match' => 'Partida de Frases Exactas',
 
-    'search_exact_word' => 'Search for Exact Word',
+    'search_exact_word' => 'Buscar palabra exacta',
 
-    'search_excerpt' => 'Excerpt',
+    'search_excerpt' => 'Fragmento',
 
-    'search_in' => 'Search In',
+    'search_in' => 'Buscar en',
 
-    'search_in_articles' => 'Search in Articles',
+    'search_in_articles' => 'Buscar en artículos',
 
-    'search_in_channels' => 'Search in Channels',
+    'search_in_channels' => 'Buscar en canales',
 
-    'search_in_titles' => 'Search in Titles',
+    'search_in_titles' => 'Buscar en Títulos',
 
-    'search_in_titles_articles' => 'Search in Titles and Articles',
+    'search_in_titles_articles' => 'Buscar en títulos y artículos',
 
-    'search_keywords' => 'Keywords',
+    'search_keywords' => 'Palabras clave',
 
-    'search_min_length' => 'Search terms must be at least %x characters in length',
+    'search_min_length' => 'Los términos de búsqueda deben tener al menos %x caracteres de longitud',
 
-    'search_max_length' => 'Search terms must be no more than %x characters in length',
+    'search_max_length' => 'Los términos de búsqueda no deben tener más de %x caracteres de longitud',
 
-    'search_module_description' => 'Search module',
+    'search_module_description' => 'Buscar módulo',
 
-    'search_module_name' => 'Search',
+    'search_module_name' => 'Buscar',
 
-    'search_most_comments' => 'Most Comments',
+    'search_most_comments' => 'Más comentarios',
 
-    'search_newer' => 'Newer',
+    'search_newer' => 'Más tarde',
 
-    'search_no_keywords' => 'You did not submit a search term',
+    'search_no_keywords' => 'No has enviado un término de búsqueda',
 
-    'search_no_result' => 'Your search did not return any results.',
+    'search_no_result' => 'Su búsqueda no devolvió ningún resultado.',
 
-    'search_no_stopwords' => 'Your search term included common words that are not allowed to be searched.',
+    'search_no_stopwords' => 'Su término de búsqueda incluye palabras comunes que no están permitidas para ser buscadas.',
 
-    'search_not_allowed' => 'You are not permitted to use the search engine.',
+    'search_not_allowed' => 'No está autorizado a utilizar el motor de búsqueda.',
 
-    'search_older' => 'Older',
+    'search_older' => 'Anterior',
 
-    'search_one_month_ago_and' => '1 Month Ago and...',
+    'search_one_month_ago_and' => 'Hace 1 mes y...',
 
-    'search_one_year_ago_and' => '1 Year Ago and...',
+    'search_one_year_ago_and' => 'Hace 1 año y...',
 
-    'search_path_error' => 'The search path contains an error. Please contact the system administrator.',
+    'search_path_error' => 'La ruta de búsqueda contiene un error. Por favor, póngase en contacto con el administrador del sistema.',
 
-    'search_recent_comment' => 'Most Recent Comment',
+    'search_recent_comment' => 'Comentario más reciente',
 
-    'search_recent_comment_date' => 'Recent Comment',
+    'search_recent_comment_date' => 'Comentario reciente',
 
-    'search_result_heading' => 'Search Results:',
+    'search_result_heading' => 'Resultados de búsqueda:',
 
-    'search_results' => 'Search Results',
+    'search_results' => 'Resultados de búsqueda',
 
-    'search_six_months_ago_and' => '6 Months Ago and...',
+    'search_six_months_ago_and' => 'Hace 6 meses y...',
 
-    'search_sort_results_by' => 'Sort Results By',
+    'search_sort_results_by' => 'Ordenar resultados por',
 
-    'search_this_week_and' => 'This Week and...',
+    'search_this_week_and' => 'Esta semana y...',
 
-    'search_three_months_ago_and' => '3 Months Ago and...',
+    'search_three_months_ago_and' => 'Hace 3 meses y...',
 
-    'search_time_not_expired' => 'You are only allowed to search every %x seconds.',
+    'search_time_not_expired' => 'Sólo puedes buscar cada %x segundos.',
 
-    'search_title' => 'Title',
+    'search_title' => 'Título',
 
-    'search_today_and' => 'Today and...',
+    'search_today_and' => 'Hoy y...',
 
-    'search_total_comments' => 'Total Comments',
+    'search_total_comments' => 'Comentarios totales',
 
-    'search_total_results' => 'Total Search Results:',
+    'search_total_results' => 'Resultados de la búsqueda total:',
 
-    'search_type' => 'Search Type',
+    'search_type' => 'Tipo de búsqueda',
 
-    'searched_for' => 'Searched for:',
+    'searched_for' => 'Buscado:',
 
-    'switch_to_group' => 'Switch to Group Search',
+    'switch_to_group' => 'Cambiar a Búsqueda de Grupo',
 
-    'switch_to_name' => 'Switch to Name Search',
+    'switch_to_name' => 'Cambiar a Buscar Nombre',
 
-    'today' => 'Today',
+    'today' => 'Hoy',
 
 );
 
